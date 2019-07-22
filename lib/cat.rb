ac6f74563fc6902c3f2bb2571a72052e10165d6c
Andrew Bay <daddnbay@yahoo.com>
2019-07-22 18:57:32 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -8,13 +8,13 @@ class Cat
   def initialize(name, owner)
     @name = name
     @owner = owner
-    
+    @@all << self
     @mood = "nervous"
-    
     owner.cats << self
   end
 
   def self.all
     @@all
   end
+  
 end
