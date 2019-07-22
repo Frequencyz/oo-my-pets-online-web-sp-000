e8daa06bf87a58338c76c8638d589dfcf2142500
Andrew Bay <daddnbay@yahoo.com>
2019-07-22 18:54:25 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,20 +1,18 @@
 class Cat
+  # code goes here
   attr_reader :name
-  attr_accessor :mood, :owner
-
+  attr_accessor :owner, :mood
   @@all = []
 
   def initialize(name, owner)
     @name = name
     @owner = owner
-    @@all << self
     @mood = "nervous"
-    @owner.cats << self
-    
+    @@all << self
+    owner.cats << self
   end
 
   def self.all
     @@all
   end
-  
 end
